a8ffb64dda81de249f8b4bf112aeeba53661c8f1
Pohl Longsine <pohl.longsine@gmail.com>
2014-07-04 23:31:20 -0500

    Initial Commit

---
+++ Swava/Swava.h
@@ -0,0 +1,19 @@
+//
+//  Swava.h
+//  Swava
+//
+//  Created by pohl on 7/4/14.
+//  Copyright (c) 2014 pohl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Swava.
+FOUNDATION_EXPORT double SwavaVersionNumber;
+
+//! Project version string for Swava.
+FOUNDATION_EXPORT const unsigned char SwavaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swava/PublicHeader.h>
+
+
---
+++ SwavaTests/SwavaTests.swift
@@ -0,0 +1,35 @@
+//
+//  SwavaTests.swift
+//  SwavaTests
+//
+//  Created by pohl on 7/4/14.
+//  Copyright (c) 2014 pohl. All rights reserved.
+//
+
+import XCTest
+
+class SwavaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
